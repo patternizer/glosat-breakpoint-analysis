15b847d090c4ccb7fa6cf85c0b44703b3ab7cd49
patternizer <michael.a.taylor@uea.ac.uk>
2021-12-19 01:00:04 +0000

    Code tidy-up

--- linear_tree_regression.py
+++ linear_tree_regression.py
@@ -82,10 +82,11 @@ import filter_cru_dft as cru_filter # CRU DFT filter
 #-----------------------------------------------------------------------------
 
 max_depth = 9 # in range [1,20]
-min_separation = 120
-min_slope_change = 6 # CUSUM / decade
+min_separation = 60 # 5 yr
+min_slope_change = 3 * (120/min_separation) 	# units: CUSUM / decade    
+min_correlation_change = 0.001 
 
-stationcode = '037401'     # HadCET
+#stationcode = '037401'     # HadCET
 #stationcode = '103810'     # Berlin-Dahlem (breakpoint: 1908)
 #stationcode = '685880'     # Durban/Louis Botha (breakpoint: 1939)
 #stationcode = '024581'     # Uppsala
@@ -94,6 +95,7 @@ stationcode = '037401'     # HadCET
 #stationcode = '260630'     # De Bilt
 #stationcode = '688177'     # Cape Town
 #stationcode = '619930'     # Pamplemousses
+stationcode = '038940'     # Guernsey
 
 #if stationcode == '103810':
 #    documented_change = 1908
@@ -105,6 +107,7 @@ documented_change = np.nan
 
 fontsize = 16
 use_dark_theme = False
+use_smoothing = True
 
 plot_ltr_correlation = True
 plot_ltr_loop = True
@@ -119,8 +122,6 @@ nfft = 10                     # decadal smoothing
 if use_dark_theme == True:
     
     matplotlib.rcParams['text.usetex'] = False
-#    rcParams['font.family'] = ['DejaVu Sans']
-#    rcParams['font.sans-serif'] = ['Avant Garde']
     rcParams['font.family'] = 'sans-serif'
     rcParams['font.sans-serif'] = ['Avant Garde', 'Lucida Grande', 'Verdana', 'DejaVu Sans' ]
     plt.rc('text',color='white')
@@ -140,8 +141,6 @@ if use_dark_theme == True:
 else:
 
     matplotlib.rcParams['text.usetex'] = False
-#    rcParams['font.family'] = ['DejaVu Sans']
-#    rcParams['font.sans-serif'] = ['Avant Garde']
     rcParams['font.family'] = 'sans-serif'
     rcParams['font.sans-serif'] = ['Avant Garde', 'Lucida Grande', 'Verdana', 'DejaVu Sans' ]
     plt.rc('text',color='black')
@@ -211,34 +210,40 @@ def adjusted_r_squared(x,y):
 
     return R2adj
 
-def calculate_piecewise_regression( x, y, depth ):
+def calculate_piecewise_regression( x, y, depth, min_separation ):
     
     # FIT: linear tree regressor
-        
-    min_samples_leaf = 120
-#   max_bins = int(len(x)/60) # range=[10,120]
-    max_bins = 40 # range=[10,120]
-        
+                
+    max_bins = int( min_separation/3 ) 			# 1/3 of min_samples_leaf in range[10,120]
+    if max_bins < 10: max_bins = 10
+    if max_bins > 120: max_bins = 120
+    min_samples_leaf = int( min_separation * (120/min_separation) ) # 1 decade
+
+    mask = np.isfinite(y)
+    x_obs = np.arange( len(y) ) / len(y)
+    x_obs = x_obs[mask].reshape(-1, 1)
+    y_obs = y[mask].reshape(-1, 1)		     
+    
     lt = LinearTreeRegressor(
         base_estimator = LinearRegression(),
         min_samples_leaf = min_samples_leaf,
         max_bins = max_bins,
         max_depth = depth        
-    ).fit(x, y)    
-    y_fit = lt.predict(x)           
+    ).fit(x_obs, y_obs)    
+    y_fit = lt.predict(x_obs)           
         
     # FIT: decision tree regressor
 
     dr = DecisionTreeRegressor(   
        max_depth = depth
-    ).fit(x, y)
-    x_fit = dr.predict(x)
+    ).fit(x_obs, y_obs)
+    x_fit = dr.predict(x_obs)
 
     # COMPUTE: goodness of fit
 
-    mask_ols = np.isfinite(y) & np.isfinite(y_fit.reshape(-1,1))
-    corrcoef = scipy.stats.pearsonr(y[mask_ols], y_fit.reshape(-1,1)[mask_ols])[0]
-    R2adj = adjusted_r_squared(y, y_fit.reshape(-1,1))
+    mask_ols = np.isfinite(y_obs) & np.isfinite(y_fit.reshape(-1,1))
+    corrcoef = scipy.stats.pearsonr(y_obs[mask_ols], y_fit.reshape(-1,1)[mask_ols])[0]
+    R2adj = adjusted_r_squared(y_obs, y_fit.reshape(-1,1))
     
     return x_fit, y_fit, corrcoef, R2adj
  
@@ -284,8 +289,28 @@ def smooth_fft(x, span):
    
 df_temp = pd.read_pickle('DATA/df_temp_expect_reduced.pkl', compression='bz2')
 df_compressed = df_temp[ df_temp['stationcode'] == stationcode ].sort_values(by='year').reset_index(drop=True).dropna()
-t_yearly = np.arange( df_compressed.iloc[0].year, df_compressed.iloc[-1].year + 1)
-df_yearly = pd.DataFrame({'year':t_yearly})
+
+#['year', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+#       'stationcode', 'stationlat', 'stationlon', 'stationelevation',
+#       'stationname', 'stationcountry', 'stationfirstyear', 'stationlastyear',
+#       'stationsource', 'stationfirstreliable', 'n1', 'n2', 'n3', 'n4', 'n5',
+#       'n6', 'n7', 'n8', 'n9', 'n10', 'n11', 'n12', 'e1', 'e2', 'e3', 'e4',
+#       'e5', 'e6', 'e7', 'e8', 'e9', 'e10', 'e11', 'e12', 's1', 's2', 's3',
+#       's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12'],
+#      dtype='object')
+
+#t_yearly = np.arange( df_compressed.iloc[0].year, df_compressed.iloc[-1].year + 1)
+#df_yearly = pd.DataFrame({'year':t_yearly})
+#df = df_yearly.merge(df_compressed, how='left', on='year')
+#dt = df.groupby('year').mean().iloc[:,0:12]
+#dn_array = np.array( df.groupby('year').mean().iloc[:,19:31] )
+#dn = dt.copy()
+#dn.iloc[:,0:] = dn_array
+#da = (dt - dn).reset_index()
+#de = (df.groupby('year').mean().iloc[:,31:43]).reset_index()
+#ds = (df.groupby('year').mean().iloc[:,43:55]).reset_index()        
+
+df_yearly = pd.DataFrame({'year': np.arange( 1780, 2021 )}) # 1780-2020 inclusive
 df = df_yearly.merge(df_compressed, how='left', on='year')
 dt = df.groupby('year').mean().iloc[:,0:12]
 dn_array = np.array( df.groupby('year').mean().iloc[:,19:31] )
@@ -293,28 +318,46 @@ dn = dt.copy()
 dn.iloc[:,0:] = dn_array
 da = (dt - dn).reset_index()
 de = (df.groupby('year').mean().iloc[:,31:43]).reset_index()
-ds = (df.groupby('year').mean().iloc[:,43:55]).reset_index()        
+ds = (df.groupby('year').mean().iloc[:,43:55]).reset_index()     
+
+# TRIM: to start of Pandas datetime range
+        
+da = da[da.year >= 1678].reset_index(drop=True)
+de = de[de.year >= 1678].reset_index(drop=True)
+ds = ds[ds.year >= 1678].reset_index(drop=True)
+
+# EXTRACT: monthly timeseries
+
 ts_monthly = np.array( da.groupby('year').mean().iloc[:,0:12]).ravel().astype(float)
 ex_monthly = np.array( de.groupby('year').mean().iloc[:,0:12]).ravel().astype(float)    
-sd_monthly = np.array( ds.groupby('year').mean().iloc[:,0:12]).ravel().astype(float)                   
-t_monthly = pd.date_range(start=str(da['year'].iloc[0]), periods=len(ts_monthly), freq='M')     
+sd_monthly = np.array( ds.groupby('year').mean().iloc[:,0:12]).ravel().astype(float)       
+
+# SET: monthly and seasonal time vectors
+
+t_monthly = pd.date_range(start=str(da['year'].iloc[0]), periods=len(ts_monthly), freq='MS')     
+t_seasonal = [ pd.to_datetime( str(da['year'].iloc[i+1])+'-01-01') for i in range(2020-1780) ] # Timestamp('YYYY-01-01 00:00:00')]
     
 # COMPUTE: 12-m MA
     
-a = pd.Series(ts_monthly).rolling(12, center=True).mean().values
-e = pd.Series(ex_monthly).rolling(12, center=True).mean().values
-s = pd.Series(sd_monthly).rolling(12, center=True).mean().values
-        
-diff_monthly = ts_monthly - ex_monthly
-diff_yearly = a - e
+if use_smoothing == True:
     
-# CALCULATE: CUSUM
-        	
+    a = pd.Series(ts_monthly).rolling(12, center=True).mean().values
+    e = pd.Series(ex_monthly).rolling(12, center=True).mean().values
+    s = pd.Series(sd_monthly).rolling(12, center=True).mean().values
+
+else:
+
+    a = ts_monthly
+    e = ex_monthly
+    s = sd_monthly
+    
+d = a - e # difference
 t = t_monthly
-c = np.nancumsum( diff_yearly )
     
-x = ( np.arange(len(c)) / len(c) ).reshape(-1, 1)
-y = c.reshape(-1, 1)
+# COMPUTE: CUSUM
+        	
+y = np.nancumsum( d )
+x = ( np.arange(len(y)) / len(y) )
 
 if np.isnan(documented_change):
     documented_change_datetime = np.nan
@@ -335,19 +378,19 @@ r = []
 r2adj = []
 for depth in range(1,max_depth+1):       
        
-    x_fit, y_fit, corrcoef, R2adj = calculate_piecewise_regression( x, y, depth )    
+    x_fit, y_fit, corrcoef, R2adj = calculate_piecewise_regression( x, y, depth, min_separation )    
     
     r.append(corrcoef)
     r2adj.append(R2adj)    
                
 r_diff = np.array( [np.nan] + list(np.diff(r)) )
-max_depth_optimum = np.arange(1,max_depth+1)[ r_diff < 0.001 ][0] - 1
+max_depth_optimum = np.arange(1,max_depth+1)[ r_diff <= min_correlation_change ][0] - 1
 
 #------------------------------------------------------------------------------
 # BEST: case
 #------------------------------------------------------------------------------
 
-x_fit, y_fit, corrcoef, R2adj = calculate_piecewise_regression( x, y, max_depth_optimum )    
+x_fit, y_fit, corrcoef, R2adj = calculate_piecewise_regression( x, y, max_depth_optimum, min_separation )    
 y_fit_diff2, slopes, breakpoints = calculate_breakpoints( y, y_fit, min_separation, min_slope_change )
 
 # CALCULATE: intra-breakpoint fragment means
@@ -356,14 +399,14 @@ y_means = []
 adjustments = []
 for j in range(len(breakpoints)+1):                
     if j == 0:              
-        y_means = y_means + list( np.tile( -np.nanmean(diff_yearly[0:breakpoints[j]]), breakpoints[j] ) ) 
-        adjustment = [ -np.nanmean(diff_yearly[0:breakpoints[j]]) ]
+        y_means = y_means + list( np.tile( -np.nanmean( d[0:breakpoints[j]] ), breakpoints[j] ) ) 
+        adjustment = [ -np.nanmean( d[0:breakpoints[j]] ) ]
     if (j > 0) & (j<len(breakpoints)):
-        y_means = y_means + list( np.tile( -np.nanmean(diff_yearly[breakpoints[j-1]:breakpoints[j]]), breakpoints[j]-breakpoints[j-1] )) 
-        adjustment = [ -np.nanmean(diff_yearly[breakpoints[j-1]:breakpoints[j]]) ]
+        y_means = y_means + list( np.tile( -np.nanmean( d[breakpoints[j-1]:breakpoints[j]] ), breakpoints[j]-breakpoints[j-1] )) 
+        adjustment = [ -np.nanmean( d[breakpoints[j-1]:breakpoints[j]] ) ]
     if (j == len(breakpoints)):              
-        y_means = y_means + list( np.tile( -np.nanmean(diff_yearly[breakpoints[-1]:]), len(diff_yearly)-breakpoints[-1] ) ) 
-        adjustment = [ -np.nanmean(diff_yearly[breakpoints[-1]:]) ]
+        y_means = y_means + list( np.tile( -np.nanmean( d[breakpoints[-1]:] ), len(d)-breakpoints[-1] ) ) 
+        adjustment = [ -np.nanmean( d[breakpoints[-1]:] ) ]
     adjustments.append(adjustment)
         
 y_means = np.array( y_means ) 
@@ -407,13 +450,13 @@ if plot_ltr_correlation == True:
     plt.plot(np.arange( 1, max_depth+1), r2adj, marker='o', ms=6, ls='-', lw=1, color='lightblue', alpha=1, label=r'$R^{2}_{adj}$')
     plt.plot( max_depth_optimum, r[max_depth_optimum-1], marker='o', ms=12, ls='-', lw=3, color='teal', alpha=0.5, label=r'$\rho$( BEST )')
     plt.plot( max_depth_optimum, r2adj[max_depth_optimum-1], marker='o', markersize=12, ls='-', lw=3, color='lightblue', alpha=0.5, label=r'$R^{2}_{adj}$( BEST )')
-    ax.xaxis.grid(b=None, which='major', color='none', linestyle='-')
-    ax.yaxis.grid(b=None, which='major', color='none', linestyle='-')
-    plt.grid(b=None)
+    ax.xaxis.grid(visible=None, which='major', color='none', linestyle='-')
+    ax.yaxis.grid(visible=None, which='major', color='none', linestyle='-')
+    plt.grid(visible=None)
     plt.xticks(np.arange(1,max_depth+1))
-    ax.xaxis.grid(b=None, which='major', color='none', linestyle='-')
-    ax.yaxis.grid(b=None, which='major', color='none', linestyle='-')
-    plt.grid(b=None)
+    ax.xaxis.grid(visible=None, which='major', color='none', linestyle='-')
+    ax.yaxis.grid(visible=None, which='major', color='none', linestyle='-')
+    plt.grid(visible=None)
     plt.tick_params(labelsize=fontsize)  
     plt.xlabel('Depth', fontsize=fontsize)
     plt.ylabel(r'Goodness of fit', fontsize=fontsize)
@@ -436,7 +479,7 @@ if plot_ltr_loop == True:
 
 	for depth in range(1, max_depth+1):       
 
-		x_fit, y_fit, corrcoef, R2adj = calculate_piecewise_regression( x, y, depth )    
+		x_fit, y_fit, corrcoef, R2adj = calculate_piecewise_regression( x, y, depth, min_separation )    
 		y_fit_diff2, slopes, breakpoints = calculate_breakpoints( y, y_fit, min_separation, min_slope_change )
 
 		plt.subplot(nr, nc, depth)
@@ -473,9 +516,9 @@ if plot_ltr_loop == True:
 			for i in range(len(breakpoints)):
 				plt.axvline( t[breakpoints[i]], ls='dashed', lw=2, color=default_color)     
 		plt.axhline( y=0, ls='-', lw=1, color=default_color, alpha=0.2)                    
-		ax.xaxis.grid(b=None, which='major', color='none', linestyle='-')
-		ax.yaxis.grid(b=None, which='major', color='none', linestyle='-')
-		ax.grid(b=None)    
+		ax.xaxis.grid(visible=None, which='major', color='none', linestyle='-')
+		ax.yaxis.grid(visible=None, which='major', color='none', linestyle='-')
+		ax.grid(visible=None)    
 		plt.tick_params(labelsize=fontsize_multi)    
 		plt.xlabel('Year', fontsize=fontsize_multi)
 		plt.ylabel(r'CUSUM (O-E), $^{\circ}$C', fontsize=fontsize_multi)    
